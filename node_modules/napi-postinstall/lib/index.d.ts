51004d5dc75e47cabf88fa6b18c83e1f93457541
yash-954 <yysep2002@gmail.com>
2025-05-13 22:45:12 +0530

    Remove node_modules from Git tracking

--- node_modules/napi-postinstall/lib/index.d.ts
+++
@@ -1,4 +0,0 @@
-import type { PackageJson } from './types.js';
-export type * from './types.js';
-export declare function isNpm(): boolean;
-export declare function checkAndPreparePackage(packageNameOrPackageJson: PackageJson | string, checkVersion?: boolean): Promise<void>;
